b4087ccc7d1388a4f62ab0da7883631237f046a9
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2024-12-29 12:24:24 +0300

    changed

--- js/task-1.js
+++ js/task-1.js
@@ -1,22 +1,9 @@
-function makeTransaction(quantity, pricePerDroid) {
-    let totalPrice = quantity * pricePerDroid;
-    console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!"`)  
-    
-}
-makeTransaction(5, 3000);
-makeTransaction(3, 1000);
-makeTransaction(10,500);
-
-/*function makeTransaction(quantity, pricePerDroid) {
-    let totalPrice = quantity * pricePerDroid;
-    console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!"`)  
-    
-}
-makeTransaction(3, 1000);
 
 function makeTransaction(quantity, pricePerDroid) {
-    let totalPrice = quantity * pricePerDroid;
-    console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!"`)  
-    
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
-makeTransaction(10,500);*/
+console.log(makeTransaction(5, 3000)); 
+console.log(makeTransaction(3, 1000)); 
+console.log(makeTransaction(10, 500));
+
